406aef0e00b9e773f6a3ea5ff945f86469c337b1
Richard Reyes <chard.dr.reyes@gmail.com>
2022-09-29 06:08:24 +0800

    escape pattern issue

--- pages/index.js
+++ pages/index.js
@@ -14,35 +14,6 @@ export default function Home({movies}) {
       </Head>
 
       <main className={styles.main}>
-        {/* <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div> */}
         <div className={styles.title}>What's Popular?</div>
         <PopularMovies movieList={movies} />
       </main>
@@ -70,7 +41,7 @@ export async function getServerSideProps(){
   //I'm using .env.local API_KEY=moviedbapikey
   const request = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&page=1`);
   const apiData = await request.json();
-  console.log('Authored by Chard Reyes');
+  console.log("Authored by Chard Reyes");
   return {
       props: {movies: apiData},
   }
